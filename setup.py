47a73273a88e1df2d8ff7f7671a763efeada9343
Jyh-Miin Lin <jyhmiinlin@gmail.com>
2019-11-03 10:05:52 +0100

    change the version to 2019.2.3rc1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import sys
 #if sys.version_info[0] == 2:
 #    sys.exit("Sorry, Python 2 is not supported yet")
 setup(name='pynufft',
-      version='2019.2.3',
+      version='2019.2.3rc1',
       description='Python non-uniform fast Fourier transform (PyNUFFT)',
       author='Jyh-Miin Lin',
       author_email='jyhmiinlin@gmail.com',
