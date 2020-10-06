5a81cc36c73be1a755ad3319ed36e8bda94c0267
Jyh-Miin Lin <jyhmiinlin@gmail.com>
2020-10-06 18:18:40 +0800

    clean up

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ import sys
 #if sys.version_info[0] == 2:
 #    sys.exit("Sorry, Python 2 is not supported yet")
 setup(name='pynufft',
-      version='2020.2.0',
+      version='2020.2.1',
       description='Python non-uniform fast Fourier transform (PyNUFFT)',
       author='Jyh-Miin Lin',
       author_email='jyhmiinlin@gmail.com',
       url = 'https://github.com/jyhmiinlin/pynufft', # use the URL to the github repo
       install_requires = ['numpy', 'scipy'],
-      license='LGPLv3',
+      license='LGPLv3, AGPL',
       packages=['pynufft'],
       package_dir={'pynufft':'.'},
       package_data={'pynufft':['nufft','src','tests','linalg','nufft/*','src/*','src/*/*','tests/*','example/*', 'linalg/*']},
